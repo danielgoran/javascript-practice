7584e2a7847665ddca7932c0debbb7bc070920ae
Daniel G <danig@Daniels-MacBook-Pro.local>
2019-10-09 23:46:34 +0100

    played with alert commands, we can write multimple comands on the same row but it's important to put semicolons after each command and the code will run

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-alert("Hello, World!");
-alert("How are you?");
-alert("Have a nice day!");
+/* ===== JavaScript practice  =====*/
+/* Our first JavaScript command
+The name of this command is “alert.” It creates a pop-up window with a text that we can specify.
+Inside of the parentheses we will write a text that we want to be displayed in a pop-up. */
+
+// alert("Hello, World!");
+// alert("How are you?");
+// alert("Have a nice day!");
+
+/* We can write the alerts on the same row but It's important to add semicolons after each command. 
+Now we add semicolons after each command. Then save it and refresh the page.
+And we see that everything works, because JavaScript first will run this command from
+alert till semicolon, then this alert command till semicolon and finally this.
+That’s why it’s better to follow the rule, that after command you should write a semicolon. */
+
+alert("Hello, World!"); alert("How are you?"); alert("Have a nice day!");
